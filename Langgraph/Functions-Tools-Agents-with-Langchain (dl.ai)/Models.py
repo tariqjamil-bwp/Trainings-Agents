af35fcae613d996f8fe015607e841218a27f9aec
TARIQ JAMIL <TariqJamil.Bwp@Gmail.Com>
2025-09-06 07:50:07 +0500

    Add files via upload
    All courses done

---
+++ Langgraph/Functions-Tools-Agents-with-Langchain (dl.ai)/Models.py
@@ -0,0 +1,39 @@
+import os
+from langchain_groq import ChatGroq
+class GroqChatLLM(ChatGroq):
+    def __init__(self, temperature=0, model_name="llama-3.1-70b-versatile", api_key=None):
+        api_key = api_key or os.environ.get("GROQ_API_KEY")
+        
+        if not api_key:
+            raise ValueError("GROQ_API_KEY must be provided either as an argument or in the environment variables.")
+        
+        # Directly call the superclass (ChatGroq) initialization
+        super().__init__(
+            temperature=temperature,
+            model_name=model_name,
+            api_key=api_key
+        )
+######################################################################################################################
+from langchain_ollama import ChatOllama
+class OllamaLLM(ChatOllama):
+    def __init__(self, temperature=0, model_name="llama3.1"):
+        # Directly call the superclass (ChatOllama) initialization with the fixed base URL
+        super().__init__(
+            temperature=temperature,
+            model=model_name,
+        )
+
+######################################################################################################################
+if __name__=="__main__":
+    os.system("clear")
+
+    llm = GroqChatLLM()
+    print('Groq LLM output:')
+    print(llm.invoke("hello").content)
+
+    # Example usage
+    llm = OllamaLLM()
+    print('Ollama LLM output:')
+    print(llm.invoke("hello").content)
+
+
---
+++ Langgraph/Langgraph_Agents/utils/__init__.py
@@ -0,0 +1,3 @@
+from .aisModels_lc import GroqChatLLM
+from .aisModels_lc import OllamaLLM
+__all__ = ["GroqChatLLM", "OllamaLLM"]
